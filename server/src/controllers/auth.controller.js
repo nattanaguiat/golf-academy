cf0530eefec5f14b04ebeac0e703f2c22af18a50
Nattan Aguia <nattan23@msn.com>
2025-06-21 21:12:09 -0400

    login controller added to the auth controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -6,24 +6,53 @@ export const signUp = async (req, res) => {
   const { name, email, password, role } = req.body;
 
   try {
+    const passwordHashed = await bcryptjs.hash(password, 10);
+
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return res.status(400).json({ message: "User already exist" });
     }
 
-    const hashedPassword = await bcryptjs.hash(password, 10);
-
     const newUser = new User({
       name,
       email,
-      password: hashedPassword,
+      password: passwordHashed,
       role,
     });
 
-    const savedUser = newUser.save();
+    const userSaved = await newUser.save();
 
-    res.status(201), json(savedUser);
+    res.status(201).json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne(email);
+    if (!userFound) return res.status(401).json({ message: "User not found" });
+
+    const isMatch = await bcryptjs.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      {
+        id: userSaved._id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("token", token, { httpOnly: true });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
